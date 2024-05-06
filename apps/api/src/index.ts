c609167e5146aaf0783bdbf09e0c191cffa886e1
guivicentep <guivicente.gvp@hotmail.com>
2024-05-06 18:54:42 -0300

    feat: Criando setup do banco de dados, e rota cadastro de usuário

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbiltyFor, projectSchema } from '@saas/auth'
-
-const ability = defineAbiltyFor({ role: 'MEMBER', id: 'user.id' })
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
-
-console.log(ability.can('get', 'Billing'))
-console.log(ability.can('create', 'Invite'))
-console.log(ability.can('delete', 'Project'))
