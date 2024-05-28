ce1a77dcd88e047d5d816c5a923d08971d3b8dbd
guivicentep <guivicente.gvp@hotmail.com>
2024-05-28 16:34:15 -0300

    feat: Listagem, detalhe e atualização da organização

---
+++ apps/api/src/utils/get-user-permissions.ts
@@ -0,0 +1,12 @@
+import { defineAbiltyFor, Role, userSchema } from '@saas/auth'
+
+export function getUserPermissions(userId: string, role: Role) {
+  const authUser = userSchema.parse({
+    id: userId,
+    role,
+  })
+
+  const ability = defineAbiltyFor(authUser)
+
+  return ability
+}
