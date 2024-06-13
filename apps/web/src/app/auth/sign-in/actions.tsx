90beca74fa778cb780690ef9335b0ff8cb3dca3a
guivicentep <guivicente.gvp@hotmail.com>
2024-06-12 22:17:38 -0300

    feat: Usando o hook useActionState

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,5 +1,23 @@
 'use server'
 
-export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+export async function signInWithEmailAndPassword(
+  previousState: unknown,
+  data: FormData,
+) {
+  console.log(previousState)
+
+  const { email, password } = Object.fromEntries(data)
+
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  console.log(result)
+
+  return 'Sucesso!'
 }
